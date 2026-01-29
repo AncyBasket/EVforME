c76181dbb6561fd3c12af51afe392aaa67444e64
Ancellotti Andrea <andrea.ancellotti@gmail.com>
2026-01-29 15:55:11 +0100

    Initial Commit

---
+++ EVforME?/EVforME_App.swift
@@ -0,0 +1,17 @@
+//
+//  EVforME_App.swift
+//  EVforME?
+//
+//  Created by Andrea Ancellotti on 29/01/26.
+//
+
+import SwiftUI
+
+@main
+struct EVforME_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ EVforME?Tests/EVforME_Tests.swift
@@ -0,0 +1,17 @@
+//
+//  EVforME_Tests.swift
+//  EVforME?Tests
+//
+//  Created by Andrea Ancellotti on 29/01/26.
+//
+
+import Testing
+@testable import EVforME_
+
+struct EVforME_Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
